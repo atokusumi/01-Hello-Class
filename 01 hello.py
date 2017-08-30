286a90585bcaa010e8d9d58ea2be21597ee3dadf
atokusumi <31392375+atokusumi@users.noreply.github.com>
2017-08-30 10:06:45 -0400

    add answers

--- 01 hello.py
+++ 01 hello.py
@@ -4,4 +4,9 @@ import sys
 
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-print('Hello, Class!')
+print('Hello, my name is Akane Tokusumi!')
+print('My favorite game is Fire Emblems.')
+print('My concern to this class is that it is in the early morning.')
+print('I am excited to be able to understand more of what the programmers are talking about')
+print('My stackoverflow.com user number is 8524420.')
+print('My github.com profile URL is https://github.com/atokusumi.')
